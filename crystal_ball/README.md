d5ae8ea99d52c02df649921144281fccecb649ff
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-04-24 16:56:27 +0200

    Crystal ball - Include extermely simple example of drawing a dungeon room

--- crystal_ball/README.md
+++ crystal_ball/README.md
@@ -2,3 +2,9 @@ Crystal ball
 ---
 
 ![](https://github.com/AleksanderGondek/rise-and-fall/workflows/crystal_ball/badge.svg)
+
+
+## Attributions
+
+* Visual assets in this project
+  * [DawnLike tiles set](https://opengameart.org/content/dawnlike-16x16-universal-rogue-like-tileset-v181), special thanks to DawnBringer
--- crystal_ball/src/main.ts
+++ crystal_ball/src/main.ts
@@ -1,10 +1,56 @@
 import * as ex from "excalibur";
 import * as _ from "lodash";
+import { Color, DisplayMode } from "excalibur";
 
-var game = new ex.Engine({
-  width: 800,
-  height: 600,
+const resources = {
+  floorTile: new ex.Texture("/assets/walls/floor_tile.png"),
+  wallTop: new ex.Texture("/assets/walls/border_top.png"),
+  wallCornerTopLeft: new ex.Texture("/assets/walls/border_top_left.png"),
+  wallCornerTopRight: new ex.Texture("/assets/walls/border_top_right.png"),
+  wallMiddle: new ex.Texture("/assets/walls/border_middle.png"),
+  wallCornerBottom: new ex.Texture("/assets/walls/border_top.png"),
+  wallCornerBottomLeft: new ex.Texture("/assets/walls/border_bottom_left.png"),
+  wallCornerBottomRight: new ex.Texture("/assets/walls/border_bottom_right.png"),
+};
+
+const loader = new ex.Loader(_.flatMap(resources));
+loader.playButtonText = "Descend into darkness..";
+loader.backgroundColor = "darkgrey";
+
+const game = new ex.Engine({
+  displayMode: DisplayMode.FullScreen,
+  backgroundColor: Color.fromRGB(0,0,0,1)
 })
 
-game.start()
-console.log(_.padStart("Hello TypeScript!", 20, ">"));
+game.start(loader).then(() => {
+  _.each(_.range(60, 252, 16), x => {
+    _.each(_.range(60, 252, 16), y => {
+      const entity = new ex.Actor({
+        pos: new ex.Vector(x, y)
+      })
+      
+      if(x == 60 && y == 60) {
+        entity.addDrawing(resources.wallCornerTopLeft);
+      } else if (x == 236 && y == 60) {
+        entity.addDrawing(resources.wallCornerTopRight);
+      } else if ( x == 60 && y == 236) {
+        entity.addDrawing(resources.wallCornerBottomLeft);
+      } else if ( x == 236 && y == 236) {
+        entity.addDrawing(resources.wallCornerBottomRight);
+      } else if (x > 60 && x < 236 && y == 60) {
+        entity.addDrawing(resources.wallTop);
+      } else if (x > 60 && x < 236 && y == 236) {
+        entity.addDrawing(resources.wallTop);
+      } else if (x == 60 && y > 60 && y < 236) {
+        entity.addDrawing(resources.wallMiddle);
+      } else if (x == 236 && y > 60 && y < 236) {
+        entity.addDrawing(resources.wallMiddle);
+      } else {
+        entity.addDrawing(resources.floorTile);
+      }
+      game.add(entity);
+    });
+  });
+
+  console.log(_.padStart("Hello TypeScript!", 20, ">"));
+});
