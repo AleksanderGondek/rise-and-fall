d916d782fee3940c8d400d1e146553d72e050863
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-06-01 18:12:57 +0200

    Astral cord - Fix cargo SHA256

--- astral_cord/astral_cord_bin.nix
+++ astral_cord/astral_cord_bin.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 
 rustPlatform.buildRustPackage {
   name = "astral_cord";
-  cargoSha256 = "0y1yjj73xjgq6jlz45208iyvlkl2hai5qlyfjk52x87lpkfxli86";
+  cargoSha256 = "08farb1srdchv8lb1vd6hk4zypa5qfrhm8s602m49vp1nh4333ar";
  
   # https://github.com/NixOS/nixpkgs/issues/71195
   # target = "x86_64-unknown-linux-musl";
