aa6aac2eef6cdf3787801d3f11089ca573431907
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-07-30 15:27:23 +0200

    [Crystal Ball] - Add upsertMap capability for game state

--- crystal_ball/src/gameMap.ts
+++ crystal_ball/src/gameMap.ts
@@ -10,11 +10,14 @@ import { IGameCell, IGameMap } from "./model";
 export class GameMap extends Scene {
   private _mapDef: IGameMap;
   private _tileMap: TileMap;
+  private _nameToIndexMap: { [name: string]: number };
+  private _sprites: Sprite[];
 
   constructor(engine: Engine, mapDef: IGameMap) {
     super(engine);
 
     this._mapDef = mapDef;
+    this._nameToIndexMap = {};
     this._tileMap = new TileMap(
       0, 
       0, 
@@ -23,42 +26,57 @@ export class GameMap extends Scene {
       this._mapDef.width / this._mapDef.tileWidth, 
       this._mapDef.height / this._mapDef.tileHeight
     );
+
+    const textureNames = Object.getOwnPropertyNames(Resources.textures);
+    textureNames.forEach((name: string, index: number) => {
+      this._nameToIndexMap[name] = index;
+    });
+
+    this._sprites = A.array.map(
+      textureNames,
+      (textureName: string) => {
+        return Resources.textures[textureName].asSprite();
+    });
   }
 
   public hash(): string {
     return this._mapDef.hash;
   }
-
-  // @ts-ignore
+  
   public upsert(mapDef: IGameMap): void {
-    // TODO: Implement upsertion
-  }
-
-  // TODO: Refactor, hairy implementation
-  public onInitialize() {
-    const textureNames = Object.getOwnPropertyNames(Resources.textures);
-
-    let nameToIndexMap: { [name: string]: number } = {};
-    textureNames.forEach((name: string, index: number) => {
-      nameToIndexMap[name] = index;
+    this._mapDef.cells.forEach((gc: IGameCell) => {
+      this._tileMap.getCell(gc.x, gc.y).clearSprites();
     });
 
-    var sprites: Sprite[] = A.array.map(
-      textureNames,
-      (textureName: string) => {
-        return Resources.textures[textureName].asSprite();
-    });
-
+    // TODO: Find cleaner way of approaching this
+    this._mapDef = mapDef;    
     this._tileMap.registerSpriteSheet(
       this._mapDef.hash,
-      new SpriteSheet(sprites)
+      new SpriteSheet(this._sprites)
     );
 
     this._mapDef.cells.forEach((gc: IGameCell) => {
       this._tileMap.getCell(gc.x, gc.y).pushSprite(
         new TileSprite(
           this._mapDef.hash,
-          nameToIndexMap[gc.imageId]
+          this._nameToIndexMap[gc.imageId]
+        )
+      );
+    });
+  }
+
+  // TODO: Refactor, hairy implementation
+  public onInitialize() {
+    this._tileMap.registerSpriteSheet(
+      this._mapDef.hash,
+      new SpriteSheet(this._sprites)
+    );
+
+    this._mapDef.cells.forEach((gc: IGameCell) => {
+      this._tileMap.getCell(gc.x, gc.y).pushSprite(
+        new TileSprite(
+          this._mapDef.hash,
+          this._nameToIndexMap[gc.imageId]
         )
       );
     });
