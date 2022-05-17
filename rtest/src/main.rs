d35d6ffacab3e02460812a500ddf447ff4ff9ca7
Aleksander Gondek <gondekaleksander@protonmail.com>
2022-05-17 21:59:46 +0200

    [rtest] Playing around with tokio.rs

--- rtest/src/main.rs
+++ rtest/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use rand::Rng;
+use tokio::runtime;
+
+async fn yeet(milis: u64) {
+  println!("yeet for {} ms - enter", milis);
+  tokio::time::sleep(std::time::Duration::from_millis(milis)).await;
+  println!("yeet for {} ms - exit", milis);
 }
+
+fn main() {
+  let main_loop: runtime::Runtime = match runtime::Builder::new_current_thread()
+    .enable_time()
+    .enable_io()
+    .build() {
+      Ok(x) => x,
+      Err(_) => return
+  };
+
+  main_loop.block_on(async move {
+    let mut interval = tokio::time::interval(std::time::Duration::from_secs(1));
+    
+    loop {
+      println!("Entering loop..");
+      let mut rng = rand::thread_rng();
+
+      let _ = tokio::join!(
+        interval.tick(),
+        tokio::spawn(yeet(rng.gen_range(200..2000))),
+        tokio::spawn(yeet(rng.gen_range(200..2000))),
+        tokio::spawn(yeet(rng.gen_range(200..2000))),
+      );     
+
+      println!("Exiting loop..");
+    }
+  });
+}
+
