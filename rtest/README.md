2d6c68717a46086cc1631ad9530c7853e1b8db43
Aleksander Gondek <gondekaleksander@protonmail.com>
2022-06-02 19:01:55 +0200

    [rtest] Play around with config file

---
+++ rtest/README.md
@@ -0,0 +1,5 @@
+rtest
+---
+
+Cargo project to test out ideas and skills.
+Will be constantly re-written to match current investigation route.
--- rtest/src/main.rs
+++ rtest/src/main.rs
@@ -1,59 +1,34 @@
-use rand::Rng;
-use tokio::{runtime, sync::oneshot::error::RecvError};
+use config::{ConfigError, File};
+use serde_derive::Deserialize;
 
-async fn fibonacci(n: u64) -> Result<u64, RecvError> {
-  let (send, recv) = tokio::sync::oneshot::channel();
-
-  rayon::spawn(move || {
-    fn fibonacci_lr(n: u64, a: u64, b: u64) -> u64 {
-      match n {
-          0 => a,
-          _ => fibonacci_lr(n - 1, a + b, a),
-      }
-    }
-    let number = fibonacci_lr(n, 1, 0);
-    let _ = send.send(number);
-  });
-  
-  recv.await
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    debug: bool,
 }
 
-async fn yeet(n: u64) {
-  println!("yeet for {} - enter", n);
-  let _ = fibonacci(n).await;
-  println!("yeet for {} - exit", n);
+impl Settings {
+  pub fn new() -> Result<Settings, ConfigError> {
+    let settings = config::Config::builder()
+      .set_default(
+        "debug",
+        true
+      )?
+      .add_source(
+        File::with_name("config.toml")
+      )
+      .build()?;
+
+    settings.try_deserialize()
+  }
 }
 
 fn main() {
-  let main_loop: runtime::Runtime = match runtime::Builder::new_multi_thread()
-    .enable_time()
-    .enable_io()
-    .build() {
-      Ok(x) => x,
-      Err(_) => return
-  };
-
-  main_loop.block_on(async move {
-    let mut interval = tokio::time::interval(std::time::Duration::from_secs(1));
-    let mut counter: u64 = 0;
-    
-    loop {
-      println!("Entering loop..");
-      counter+=1;
-      if counter > 10 {
-        return;
-      }
-      let mut rng = rand::thread_rng();
-
-      let _ = tokio::join!(
-        interval.tick(),
-        tokio::spawn(yeet(rng.gen_range(20..90))),
-        tokio::spawn(yeet(rng.gen_range(20..90))),
-        tokio::spawn(yeet(rng.gen_range(20..90))),
-      );     
-
-      println!("Exiting loop..");
+  match Settings::new() {
+    Ok(settings) => println!(
+      "Configuration debug flag value: {:?}", settings.debug
+    ),
+    _ => {
+      println!("Could not read configuration file!")
     }
-  });
+  }
 }
-
