0bcc180e3775656f5f7baec28118685080037692
Aleksander Gondek <gondekaleksander@protonmail.com>
2022-07-07 21:20:59 +0200

    Switch to nixpkgs-22.05

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       url = github:edolstra/flake-compat;
       flake = false;
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
